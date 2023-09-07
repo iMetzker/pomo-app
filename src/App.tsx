4e2bedeec10d6c94ff9c95f0c115100651f566dc
Metzker <ivnymetzker@gmail.com>
2023-09-06 21:58:17 -0300

    style: adding global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/defaul";
+import { GlobalStyle } from "./styles/themes/global";
 
 export function App() {
 
-  return <h1>Teste</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Teste</h1>
+      
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
